a164d5943ac85f47f254eb1ab2fecb3568f2eb3d
minhquan0509 <minkkquan2@gmail.com>
2023-09-10 15:04:10 +0700

    [Homepage] homepage UI

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,40 @@
-export default function Topbar(){
-    return (
-        <>
-        </>
-    )
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Topbar() {
+  const isUserLoggedIn = true;
+
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      <div className="flex item-center gap-1">
+        <div className="block md:hidden" style={{ margin: "auto" }}>
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex items-center cursor-pointer gap-4 p-4">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 }
