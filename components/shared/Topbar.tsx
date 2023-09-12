12e1223a6a8bfaaea479ec568ac4c059df9df7a4
minhquan0509 <minkkquan2@gmail.com>
2023-09-12 11:44:12 +0700

    [Onboard page] onboarding UI

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 export default function Topbar() {
   const isUserLoggedIn = true;
@@ -29,6 +30,7 @@ export default function Topbar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
